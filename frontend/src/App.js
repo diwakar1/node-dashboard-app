3d666093ea2733609ecdcb7a60555d7344d4879f
Diwakar Sharma <diwakar2034@yahoo.com>
2026-01-14 21:18:31 -0600

    fixed app and  index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import PrivateComponent from "./components/PrivateComponent";
 import Login from "./components/Login"
@@ -9,24 +9,26 @@ import AddProduct from "./components/AddProduct";
 import ProductList from "./components/ProductList";
 import Update from "./components/Update";
 import Profile from "./components/Profile"
+
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+  const isSignUpPage = location.pathname === "/signup";
   return (
     <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route element={<PrivateComponent />}>
-            <Route path="/" element={<ProductList/>} />
-            <Route path="/add" element={<AddProduct/>} />
-            <Route path="/update/:id" element={<Update/>} />
-            <Route path="/logout" element={<h1>logout Component</h1>} />
-            <Route path="/profile" element={<Profile/>} />
-          </Route>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element ={<Login/>}/>
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+      <Nav />
+      <Routes>
+        <Route element={<PrivateComponent/>}>
+          <Route path="/" element={<ProductList/>} />
+          <Route path="/add" element={<AddProduct/>} />
+          <Route path="/update/:id" element={<Update/>} />
+          <Route path="/logout" element={<h1>logout Component</h1>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Route>
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/login" element ={<Login/>}/>
+      </Routes>
+      {!(isLoginPage || isSignUpPage) && <Footer />}
     </div>
   );
 }
