d82763cb62c9e1e885230d59458fe5f9a42d6145
Diwakar Sharma <diwakar2034@yahoo.com>
2026-02-12 22:04:30 -0600

    added new order routes

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,7 @@ import productRoutes from './routes/product.js';
 import refreshRoutes from './routes/refresh.js';
 import categoryRoutes from './routes/category.js';
 import dashboardRoutes from './routes/dashboard.js';
+import orderRoutes from './routes/order.js';
 
 
 const app = express();
@@ -53,6 +54,7 @@ app.use("/api/v1/auth/refresh", refreshRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/orders", orderRoutes);
 
 // Serve React frontend in production
 if (process.env.NODE_ENV === "production") {
