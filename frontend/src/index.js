1aa84a52f78a359c28430615ab849b0ea5a3863b
Diwakar Sharma <diwakar2034@yahoo.com>
2026-03-24 22:25:03 -0500

    added fonts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import './styles/fontawesome-vite-fix.css';
 import App from './App';
-
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
